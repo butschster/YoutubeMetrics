ec32aee949b99166b35c05dfd48d1a6cfb7a5182
Pavel Buchnev <butschster@gmail.com>
2018-03-18 23:05:47 +0300

    fix

--- app/Entities/Comment.php
+++ app/Entities/Comment.php
@@ -110,6 +110,6 @@ class Comment extends Model
      */
     public function author()
     {
-        return $this->belongsTo(Author::class);
+        return $this->belongsTo(Author::class, 'channel_id');
     }
 }
