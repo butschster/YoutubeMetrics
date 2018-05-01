22432df84e0a376e61e16a161f66abc74f997afa
Pavel Buchnev <butschster@gmail.com>
2018-05-01 08:40:15 +0000

    youtube.php edited online with Bitbucket

--- config/youtube.php
+++ config/youtube.php
@@ -1,11 +1,6 @@
 <?php
 
 return [
-    'keys' => [
-        'AIzaSyAL3arrCabjqZ6zrp4_aKrQAJR182T-rL0',
-        'AIzaSyCKbhASmyWUhd4CimMwk9aLlE3gqcT_yQI',
-        'AIzaSyAiXz0T2U4jPzH2I3s88Rti5J4YuaDlgxs'
-    ],
     'lifetime' => [ // in hours
         'comments' => 6, // Переиод, когда производится синхронизация комментариев к видео
         'videos' => 24, // Переиод, когда собирается статистика по видео
